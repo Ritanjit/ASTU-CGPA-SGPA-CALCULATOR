7b717ca16999f4414233120683512c41b5cbd2bb
ritanjit <ritanjitdas@gmail.com>
2025-05-23 18:30:48 +0530

    up

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer/footer";
-import FloatingQRButton from "@/components/floatingButton/floatingButton";
 
 const RootLayout: React.FC = () => {
 	return (
